45800a30ccbaf0b05c56e9f498202ca9e70722f8
Khalid <49591985+Mosallamy@users.noreply.github.com>
2021-03-30 14:46:46 +0300

    Update function_rename.js

--- function_rename.js
+++ function_rename.js
@@ -1,3 +1,9 @@
-function TestTest(){
-  console.log("Test")
-}
+function getRating(objecetTest):
+  if objecetTest.moreThanFiveLateDeliveries(){
+    return 5
+  }else{
+    return 1
+  }
+  
+function moreThanFiveLateDeliveries(objecetTest):
+  return objecetTest.numberOfLateDeliveries > 5
